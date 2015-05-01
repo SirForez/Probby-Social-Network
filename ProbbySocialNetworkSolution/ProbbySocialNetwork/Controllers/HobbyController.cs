69d555012ffe93f08de6db121986948b20199986
SirForez <orrigudlaugur@gmail.com>
2015-05-01 17:02:51 +0000

    Added functions to controllers (TODO: Implement), still need DBcontexts for controllers

--- ProbbySocialNetworkSolution/ProbbySocialNetwork/Controllers/HobbyController.cs
+++ ProbbySocialNetworkSolution/ProbbySocialNetwork/Controllers/HobbyController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProbbySocialNetwork.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,36 @@ namespace ProbbySocialNetwork.Controllers
 {
     public class HobbyController : Controller
     {
+
+        public List<Hobby> hobbiesAwaitingApproval;
+
+        public ActionResult addHobby()
+        {
+            //TODO: implement
+            return View();
+        }
+
+        public ActionResult removeHobby()
+        {
+            //TODO: implement
+            return View();
+        }
+
+        public ActionResult approveHobby()
+        {
+            //TODO: implement
+            return View();
+        }
+
+        public ActionResult viewHobby()
+        {
+            //TODO: implement
+            return View();
+        }
         // GET: Hobby
         public ActionResult Index()
         {
+            //TODO: implement
             return View();
         }
     }
--- ProbbySocialNetworkSolution/ProbbySocialNetwork/Controllers/PostController.cs
+++ ProbbySocialNetworkSolution/ProbbySocialNetwork/Controllers/PostController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProbbySocialNetwork.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,25 @@ namespace ProbbySocialNetwork.Controllers
 {
     public class PostController : Controller
     {
+        public List<Post> reportedPosts;
+        public List<Account> reportedAccounts;
+
+        public ActionResult viewStatus()
+        {
+            //TODO: implement
+            return View();
+        }
+
+        public ActionResult getPostsByAccount(Account a)
+        {
+            //TODO: implement
+            return View();
+        }
+
         // GET: Post
         public ActionResult Index()
         {
+            //TODO: implement
             return View();
         }
     }
