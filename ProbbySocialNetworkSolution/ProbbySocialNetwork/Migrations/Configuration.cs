2328e8cb58f291f41cfeb769a89a0f1c786673f8
unknown <bjarturhafthors@hotmail.com>
2015-05-07 21:11:09 +0000

    Updated database

---
+++ ProbbySocialNetworkSolution/ProbbySocialNetwork/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace ProbbySocialNetwork.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ProbbySocialNetwork.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "ProbbySocialNetwork.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(ProbbySocialNetwork.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
