46f5399de3283e9559edeabdce8360b067eebdf7
SirForez <orrigudlaugur@gmail.com>
2015-05-07 13:57:47 +0000

    Added controller, service and repo files

---
+++ ProbbySocialNetworkSolution/ProbbySocialNetwork/Models/AccountRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProbbySocialNetwork.Models
+{
+    public class AccountRepository
+    {
+    }
+}
