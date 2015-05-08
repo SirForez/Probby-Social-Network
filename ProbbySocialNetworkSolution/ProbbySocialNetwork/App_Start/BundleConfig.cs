b003c942b97970e21c8840cb87b93c3d7855a527
eythorsnaer <eythorsnaer@gmail.com>
2015-05-08 11:21:14 +0000

    feed view

--- ProbbySocialNetworkSolution/ProbbySocialNetwork/App_Start/BundleConfig.cs
+++ ProbbySocialNetworkSolution/ProbbySocialNetwork/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace ProbbySocialNetwork
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+					  "~/Content/feed.css"));
         }
     }
 }
