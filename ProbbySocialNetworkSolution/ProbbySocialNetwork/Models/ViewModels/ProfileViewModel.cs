82f3c2c3dd6e7ff319af505dc1c6d6c2df83bbed
SirForez <orrigudlaugur@gmail.com>
2015-05-08 18:35:52 +0000

    Got profile view to work with Profile() in homecontroller and profileViewModel

--- ProbbySocialNetworkSolution/ProbbySocialNetwork/Models/ViewModels/ProfileViewModel.cs
+++ ProbbySocialNetworkSolution/ProbbySocialNetwork/Models/ViewModels/ProfileViewModel.cs
@@ -9,5 +9,6 @@ namespace ProbbySocialNetwork.Models.ViewModels
     {
         public ApplicationUser currentUser;
         public List<Status> currentUserStatusHistory;
+        public List<Comment> commentsForStatuses;
     }
 }
