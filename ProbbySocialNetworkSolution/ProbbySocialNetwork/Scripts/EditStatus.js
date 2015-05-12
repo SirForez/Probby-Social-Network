2213e33e019d09c09868b6e0e3fa3bd98a0a29dd
eythorsnaer <eythorsnaer@gmail.com>
2015-05-12 19:07:29 +0000

    edit status, javascript, status controller

---
+++ ProbbySocialNetworkSolution/ProbbySocialNetwork/Scripts/EditStatus.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $(".statusContent2").show();
+});
+
+function editStatus(id) {
+    $("#statusText" + id).hide();
+    $("#statusPic" + id).hide();
+    $("#statusTextForm" + id).show();
+    $("#statusPicForm" + id).show();
+}; 
