4815ec1fbb068bddaca09828aecad96f208be358
SirForez <orrigudlaugur@gmail.com>
2015-05-13 18:22:15 +0000

    Word wrapping works, now group admins can delete the group

--- ProbbySocialNetworkSolution/ProbbySocialNetwork/Controllers/HomeController.cs
+++ ProbbySocialNetworkSolution/ProbbySocialNetwork/Controllers/HomeController.cs
@@ -32,7 +32,6 @@ namespace ProbbySocialNetwork.Controllers
             model.commentsForStatuses = new List<Comment>();
 			model.currentUserGroups = groupService.getGroupsByUser(model.currentUser);
             model.currentUserGroups.AddRange(groupService.getGroupsByAdmin(model.currentUser));
-            model.currentUserGroups.OrderBy(o => o.name);
             model.currentUserHobbies = hobbyService.getHobbiesByUser(model.currentUser);
 
 			//For statuses, we also need to add the hobbies and shit
