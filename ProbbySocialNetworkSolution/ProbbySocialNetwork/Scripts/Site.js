392a3f530315c76d176fccd8635b3ff75e4602c5
eythorsnaer <eythorsnaer@gmail.com>
2015-05-13 17:02:05 +0000

    script fix

---
+++ ProbbySocialNetworkSolution/ProbbySocialNetwork/Scripts/Site.js
@@ -0,0 +1,7 @@
+﻿var main = function () {
+    $(".onClick").click(function () {
+        $(".appendTo").append("<input type='text' name='url' size='35' maxlength='255' placeholder='Insert IMG url here'/>");
+        $(".onClick").hide();
+    });
+}
+$(document).ready(main);
