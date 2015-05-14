5da846166a381e9eb23bffb994de768f74e67c98
unknown <bjarturhafthors@hotmail.com>
2015-05-14 15:04:37 +0000

    Added hobby validation

--- ProbbySocialNetworkSolution/ProbbySocialNetwork/Models/Hobby.cs
+++ ProbbySocialNetworkSolution/ProbbySocialNetwork/Models/Hobby.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProbbySocialNetwork.Models
 {
 	public class Hobby
 	{
 		public int ID { get; set; }
+
+		[RegularExpression("^([a-zA-Z0-9]+)$", ErrorMessage = "Invalid Name")]
 		public string Name { get; set; }
 	}
 }
