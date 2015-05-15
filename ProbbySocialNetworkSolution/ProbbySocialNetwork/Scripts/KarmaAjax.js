b6891165f7ebaa964d5a921fc32fca223952021f
unknown <bjarturhafthors@hotmail.com>
2015-05-15 07:24:51 +0000

    bla

--- ProbbySocialNetworkSolution/ProbbySocialNetwork/Scripts/KarmaAjax.js
+++ ProbbySocialNetworkSolution/ProbbySocialNetwork/Scripts/KarmaAjax.js
@@ -26,27 +26,3 @@
             
     })
 });
-
-
-
-
-        var url = '/Status/UpvoteStatus';
-        alert("sjomli!!!!!");
-        $.ajax({
-            type: 'POST',
-            url: url,
-            data: {
-                movieId: movie,
-            }
-
-        }).done(function (data) {
-            alert("done!!!!!");
-            var karmaId = '#karmaDisplay' + data.ID.toString();
-            $(karmaId).html(data.Karma);
-        }).fail(function () {
-            alert('Villa kom upp!');
-        });
-
-        return false;
-    });
-});
